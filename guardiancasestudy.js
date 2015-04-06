f8d22b2a63674a9aee6b890cbbdce8d0381fab1d
g0rd <r.devashrayee@gmail.com>
2015-04-06 00:26:32 -0400

    added the data representation
    added all the data through javascript

--- guardiancasestudy.js
+++ guardiancasestudy.js
@@ -1,24 +1,26 @@
 /**
  * @author Dee's
  */
-	var htmlInput = '';
+	
 	function populatePage(){
 		for(var i = 0; i < guardianHeroinData.length; i++){
 			var individualHeroinDatum = guardianHeroinData[i];
-			htmlInput += '<div class="quoteContainer"><div class="mainQuoteContainer">'+
+			var htmlInput = '<div class="quoteContainer"><div class="mainQuoteContainer">'+
 					individualHeroinDatum.mainQuote
 				+ '</div><div class="wholeQuoteContainer">' +
 					individualHeroinDatum.wholeQuote
-				+'</div><div class="personContainer">'
-					individualHeroinDatum.person
+				+'</div><div class="personContainer">' +
+					individualHeroinDatum.source
 				+ '</div><div class="yearsCleanContainer">' +
 					individualHeroinDatum.yearsClean
 				+ '</div><div class="locationContainer">' +
 					individualHeroinDatum.location
 				+ '</div></div>';
+				$('#dataContainer').append(htmlInput);
 		}
+		
 	}
 	
 	function injectHTML(){
-		$(#dataContainer).append(htmlpInput);
+		populatePage();
 	}
